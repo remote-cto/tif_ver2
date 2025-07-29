7e14ded34144fbf474e86c8fb6e739e5c1d858f7
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-07-29 22:39:13 +0530

    video added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -169,7 +169,7 @@ const Page = () => {
                 playsInline
                 className="w-full h-full object-cover"
               >
-                <source src="/video/BgNew.mp4" type="video/mp4" />
+                <source src="/video/Bgnew.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
               <div className="absolute inset-0 bg-black/40"></div>
